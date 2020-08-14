3aa98087c436f23044706a66a1cd6079649e2762
Indranil Tiwary <>
2020-08-14 22:58:54 +0530

    Initial Commit

---
+++ event-bus/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/events', (req, res) => {
+  try {
+    const event = req.body;
+    console.log(event);
+
+    axios.post('http://localhost:4000/events', event);
+    axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4002/events', event);
+
+    res.send({ status: 'OK' });
+  } catch (e) {}
+});
+
+app.listen(4005, () => {
+  console.log('Listening on 4005');
+});
