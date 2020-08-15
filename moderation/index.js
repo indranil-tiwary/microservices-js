302935bca8191dcc11a2a96c0b1e4abb8f5011b7
Indranil Tiwary <>
2020-08-15 22:28:34 +0530

    Kubernetes Cluster and Services added

--- moderation/index.js
+++ moderation/index.js
@@ -11,7 +11,7 @@ app.post('/events', async (req, res) => {
   if (type === 'CommentCreated') {
     const { id, postId, content = '' } = data;
     const status = content.includes('orange') ? 'rejected' : 'approved';
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
       type: 'CommentModerated',
       data: {
         id,
